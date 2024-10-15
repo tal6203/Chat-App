d22e55ee42175aad5028ccffa19a477e41577c6f
Tal Abutbul <112417918+tal6203@users.noreply.github.com>
2024-10-16 02:24:45 +0300

    Update user.js

--- Backend/models/user.js
+++ Backend/models/user.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
   username: { type: String, unique: true, required: true },
   password: { type: String, required: true, minlength: 6 },
+  email: { type: String, unique: true, required: true },
   profilePicture: { type: "String", required: true, default: "https://res.cloudinary.com/dfa7zee9i/image/upload/v1715111941/anonymous-avatar_wcrklv_u0kzbb.png", },
   status: { type: String, required: true, default: "Welcome to ChatApp" },
 });
